99a14702b2413d98e064598be097e565ee05b885
Piotr <piruci88@gmail.com>
2018-04-21 10:45:52 +0200

    Scene Examples

---
+++ Assets/Scripts/NextLev.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class NextLev : MonoBehaviour {
+
+    public string Name;
+
+    void OnMouseDown()
+    {        
+        Application.LoadLevel(Name);
+    }
+}
+
